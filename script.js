425681c752ab65470a548a4c190b687abd3e5006
M.A. Asad <maasad11914@gmail.com>
2024-02-15 16:29:57 +0600

    Added Responsiveness on Home page

---
+++ script.js
@@ -0,0 +1,11 @@
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const nav = document.querySelector("ul");
+
+bar.addEventListener("click",()=>{
+    nav.style.right = "0px";
+})
+close.addEventListener("click",(event)=>{
+    event.preventDefault();
+    nav.style.right = "-300px";
+})
