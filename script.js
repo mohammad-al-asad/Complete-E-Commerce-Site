220f49685e7295a837252df4518800955c8cb228
M.A. Asad <maasad11914@gmail.com>
2024-02-17 14:15:15 +0600

    Added shop and detail page

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+// Responsive Menu
 const bar = document.getElementById("bar");
 const close = document.getElementById("close");
 const nav = document.querySelector("ul");
-
 bar.addEventListener("click",()=>{
     nav.style.right = "0px";
-})
+});
 close.addEventListener("click",(event)=>{
     event.preventDefault();
     nav.style.right = "-300px";
-})
+});
+
+// Load Detail
+function loadDetail(){
+    window.location.href = "detail.html";
+}
